0de1f7ee3b438b0ed36393c130b4b5909c1222fb
ValMoncada <89555606+Valeria705@users.noreply.github.com>
2022-09-07 20:16:19 -0500

    Add files via upload

---
+++ cinemiercoles/src/controllers/controladorCINEMA.js
@@ -0,0 +1,320 @@
+let peliculas=[
+    {
+        nombre:"Avatar",
+        genero:"Ciencia ficción",
+        duracion:220,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemiercolesjjg.appspot.com/o/avatar.png?alt=media&token=4775b8fa-5e9c-42bb-9ae3-f0f0bf010f0d",
+        sinopsis:"un viaje de aventuras, acción como relato clásico de aventuras fantásticas, Avatar funciona en varios niveles, desde la fábula ecológica y las redes sociales hasta el mito del Héroe",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Steven Spielberg",
+        actores:["Arnold Schwarzenegger"," Bruce Willis"," Jason Statham"]
+    },
+    {
+        nombre:"La Bestia",
+        genero:"suspenso",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemiercolesjjg.appspot.com/o/la%20bestia.jpg?alt=media&token=b46a53b9-3af7-4fc2-9c7e-966ec628f719",
+        sinopsis:"El Doctor Nate Samuels regresa a Sudáfrica, lugar en el que conoció a su difunta mujer, con sus dos hijas para pasar unos días en la Sabana viendo la fauna del lugar.",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Peter Jackson",
+        actores:["Laughton"," Choi Min-sik"," Chang Chen"]
+    },
+    {
+        nombre:"Tren Bala",
+        genero:"suspenso",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinevm-f98a1.appspot.com/o/trenbala.jpg?alt=media&token=5c3a8ba1-c30b-45eb-8dd9-c91f110f07f7",
+        sinopsis:"Brad Pitt protagoniza la película en el papel un asesino con poca suerte determinado a hacer su trabajo en paz después de que más de una de sus asignaciones se le han salido contro",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Michael Bay",
+        actores:["Burt Chaplin"," B.Willis"," Buster Keaton"]
+    }
+    ,
+    {
+        nombre:"Vetigo",
+        genero:"suspenso",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinevm-f98a1.appspot.com/o/vertigo.png?alt=media&token=b1660dd4-053b-42e0-8592-ba17b189a361",
+        sinopsis:"Para las mejores amigas Becky y Hunter, la vida consiste en conquistar los miedos y superar los límites",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"James Cameron",
+        actores:["Morfydd Clark"," Al Pacino"," Alfredo Landa"]
+    },
+    {
+        nombre:"Gallo de pelea",
+        genero:"Ciencia ficción",
+        duracion:220,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinevm-f98a1.appspot.com/o/c47e0a99fe9e-gallo-de-pelea-480x670.jpg?alt=media&token=4ad2d941-e244-47dc-a87a-cb8099cd7a3f",
+        sinopsis:" Un humilde cultivador, decide ponerle cara a su destino, inspirado por la valentía de un gallo de pelea que misteriosamente aparece en su vida",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"David Yates",
+        actores:["Al Pacino"," Anthony Hopkins"," Bruno Ganz"]
+    },
+    {
+        nombre:"Invitacion al infierno",
+        genero:"suspenso",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinevm-f98a1.appspot.com/o/terror.png?alt=media&token=24267354-9456-4aeb-9790-9f9933e18819",
+        sinopsis:"la historia de su familia y las inquietantes intenciones que se esconden tras su pecaminosa generosidad",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Christopher Nolan",
+        actores:["Alberto "," Alec Guinness"," Alfredo Landa"]
+    },
+    {
+        nombre:"Peleando por mi vida",
+        genero:"suspenso",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinevm-f98a1.appspot.com/o/vida.png?alt=media&token=966a0917-27b9-40e1-b0ec-134f64c96f98",
+        sinopsis:"Un boxeador que luchó contra sus compañeros prisioneros en los campos de concentración para sobrevivir.",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Robert Zemeckis",
+        actores:["Morfydd Clark"," Joy Ramirez"," Chelsea Noble"]
+    }
+    ,
+    {
+        nombre:"Erase una vez",
+        genero:"suspenso",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinevm-f98a1.appspot.com/o/caperusita.png?alt=media&token=c2d4a53c-cff0-490e-8d93-da2438e728ac",
+        sinopsis:"Una aventura fantástica donde una mujer, Alithea(Tilda Swinton) descubre un genio (Idris Elba) en una lámpara",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Tim Burton",
+        actores:["Robert Aramayo"," Morfydd Clark"," Álex Pastrana"]
+    }
+    ,
+    {
+        nombre:"Pasaje al paraiso",
+        genero:"suspenso",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinevm-f98a1.appspot.com/o/pasaje.png?alt=media&token=9275a3b0-6e29-478b-a8b4-9cafbc17c806",
+        sinopsis:" Pasaje al Paraíso es una comedia romántica sobre la dulce sorpresa de las segundas oportunidades.",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Chris Columbus",
+        actores:["Will Poulter"," Bruce Willis"," Joy Ramirez"]
+    }
+    ,
+    {
+        nombre:"La ciudad de las estrellas",
+        genero:"suspenso",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinevm-f98a1.appspot.com/o/estrellas.png?alt=media&token=2657a78b-efb8-49aa-a817-c3c75de306a8",
+        sinopsis:" Ante una ciudad que poco a poco revela una gran hostilidad, Tato ha encontrado en la música y en su pequeño grupo de amigos un refugio",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Gore Verbinski",
+        actores:["Chelsea Noble"," Candace Cameron Bure"," Jason Statham"]
+    }
+]
+
+let peliculasProximas=[
+    {
+        nombre:"Spiderman",
+        genero:"Ciencia ficción",
+        duracion:220,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinevm-f98a1.appspot.com/o/pro2.jpg?alt=media&token=eeed93bd-bd73-438c-af65-4f901d6d1e35",
+        sinopsis:"un viaje de aventuras, acción como relato clásico de aventuras fantásticas, Avatar funciona en varios niveles, desde la fábula ecológica y las redes sociales hasta el mito del Héroe",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Steven Spielberg",
+        actores:["Arnold Schwarzenegger"," Bruce Willis"," Jason Statham"]
+    },
+    {
+        nombre:"El Cataclismo",
+        genero:"suspenso",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinevm-f98a1.appspot.com/o/pro1.jpg?alt=media&token=50e43933-5349-4a53-aa86-a81acc0b97bd",
+        sinopsis:"El Doctor Nate Samuels regresa a Sudáfrica, lugar en el que conoció a su difunta mujer, con sus dos hijas para pasar unos días en la Sabana viendo la fauna del lugar.",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Peter Jackson",
+        actores:["Laughton"," Choi Min-sik"," Chang Chen"]
+    },
+    {
+        nombre:"Emergencia en el aire",
+        genero:"suspenso",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinevm-f98a1.appspot.com/o/pro3.png?alt=media&token=d3b7bb01-86bf-4e5a-afd3-c06ce0147d74",
+        sinopsis:"Brad Pitt protagoniza la película en el papel un asesino con poca suerte determinado a hacer su trabajo en paz después de que más de una de sus asignaciones se le han salido contro",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"Michael Bay",
+        actores:["Burt Chaplin"," B.Willis"," Buster Keaton"]
+    }
+    ,
+    {
+        nombre:"Barbaro",
+        genero:"suspenso",
+        duracion:120,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinevm-f98a1.appspot.com/o/pro4.png?alt=media&token=c5d5ac60-bcf9-4bd4-aeb1-10648df57cd1",
+        sinopsis:"Para las mejores amigas Becky y Hunter, la vida consiste en conquistar los miedos y superar los límites",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"James Cameron",
+        actores:["Morfydd Clark"," Al Pacino"," Alfredo Landa"]
+    },
+    {
+       
+        nombre:" No te preocupes Cariño ",
+        genero:"Romace",
+        duracion:220,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinevm-f98a1.appspot.com/o/pro5.jpg?alt=media&token=88d069d2-6def-42b0-a3f2-2064ae6216bc",
+        sinopsis:" Un humilde cultivador, decide ponerle cara a su destino, inspirado por la valentía de un gallo de pelea que misteriosamente aparece en su vida",
+        clasificacion:"+18",
+        idioma:"En subtitulado",
+        director:"David Yates",
+        actores:["Al Pacino"," Anthony Hopkins"," Bruno Ganz"]
+    }]
+
+//RECORRIENDO UN ARREGLO EN JS
+
+let fila= document.getElementById("fila");
+
+peliculas.forEach(function(pelicula){
+   
+    //  TRAVERSING (CREAR ETIQUETAS HTML DESDE JAVASCRIPT)
+    //1. creamos columna para cada pelicula
+
+    let columna= document.createElement("div")
+    columna.classList.add("col")
+
+    //2. creamos una tarjeta para cada pelicula
+
+    let tarjeta= document.createElement("div")
+    tarjeta.classList.add("card","h-100")
+
+    //3.Creamos una foto para cada pelicula
+
+    let poster=document.createElement("img")
+    poster.classList.add("card-img-top")
+    poster.src=pelicula.poster
+
+    //4.Creamos nombre de la pelicula
+    let nombrePelicula= document.createElement("h3")
+    nombrePelicula.classList.add("text-center")
+    nombrePelicula.textContent=pelicula.nombre
+
+    //5.Creamos el genero de la pelicula
+
+    let genero= document.createElement("h4")
+    genero.classList.add("text-center")
+    genero.textContent="Genero: "+pelicula.genero
+
+    //6. Creamos el idioma de cada pelicula
+    let idioma= document.createElement("h6")
+    idioma.classList.add("text-center")
+    idioma.textContent="Idioma: "+pelicula.idioma
+
+    //7.Creamso la sinopsis
+    let sinopsis= document.createElement("h6")
+    sinopsis.textContent="Sinopsis: "+pelicula.sinopsis
+
+     //8.Creamos el director
+     let director= document.createElement("h6")
+     director.classList.add("text-center")
+     director.textContent="Director: "+pelicula.director
+
+     //9.Creamos los actores
+     let actores= document.createElement("h6")
+     actores.classList.add("text-center")
+     actores.textContent="Actores: "+pelicula.actores
+ 
+
+
+    //Padres e hijos
+
+    
+    tarjeta.appendChild(poster)
+    tarjeta.appendChild(nombrePelicula)
+    tarjeta.appendChild(genero)
+    tarjeta.appendChild(idioma)
+    tarjeta.appendChild(actores)
+    tarjeta.appendChild(sinopsis)
+    tarjeta.appendChild(director)
+    columna.appendChild(tarjeta)
+    fila.appendChild(columna)
+   
+
+
+
+})
+
+let filados= document.getElementById("filados");
+
+peliculasProximas.forEach(function(pelicula){
+   
+    let columna= document.createElement("div")
+    columna.classList.add("col")
+
+
+    let tarjeta= document.createElement("div")
+    tarjeta.classList.add("card","h-100")
+
+
+    let poster=document.createElement("img")
+    poster.classList.add("card-img-top")
+    poster.src=pelicula.poster
+
+    
+    let nombrePelicula= document.createElement("h3")
+    nombrePelicula.classList.add("text-center")
+    nombrePelicula.textContent=pelicula.nombre
+
+   
+
+    let genero= document.createElement("h4")
+    genero.classList.add("text-center")
+    genero.textContent="Genero: "+pelicula.genero
+
+    
+    let idioma= document.createElement("h6")
+    idioma.classList.add("text-center")
+    idioma.textContent="Idioma: "+pelicula.idioma
+
+    
+    let sinopsis= document.createElement("h6")
+    sinopsis.textContent="Sinopsis: "+pelicula.sinopsis
+
+   
+     let director= document.createElement("h6")
+     director.classList.add("text-center")
+     director.textContent="Director: "+pelicula.director
+
+
+     let actores= document.createElement("h6")
+     actores.classList.add("text-center")
+     actores.textContent="Actores: "+pelicula.actores
+ 
+
+
+
+    
+    tarjeta.appendChild(poster)
+    tarjeta.appendChild(nombrePelicula)
+    tarjeta.appendChild(genero)
+    tarjeta.appendChild(idioma)
+    tarjeta.appendChild(actores)
+    tarjeta.appendChild(sinopsis)
+    tarjeta.appendChild(director)
+    columna.appendChild(tarjeta)
+    filados.appendChild(columna)
+   
+
+
+
+})
+
+
+
+//DETECTANDO SELECION DE UNA PELICULA
+fila.addEventListener("click", function(){
+    alert("esta seleccionando una pelicula")
+})
